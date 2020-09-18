22721a6d3eb6b2f2ddca29eb98ee1424e85fc8e8
Ray Bateman <rayb@gradecam.com>
2020-09-18 10:43:55 -0600

    If modGen is reloaded during runtime make sure we reload all the modules

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 
+reloadModGen(); // used during development if you reload this module it'll reload the other modules too
 export {
     moduleDef,
     writeModule,
@@ -10,3 +11,12 @@ export {
 export {
     ModGenPlugin, ModGenPluginOptions,
 } from './webpackPlugin';
+
+function reloadModGen() {
+    for (const path of [
+        require.resolve('./matchers'),
+        require.resolve('./modGen'),
+    ]) {
+        delete require.cache[require.resolve(path)];
+    }
+}
